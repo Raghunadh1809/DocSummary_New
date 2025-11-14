b21b28cd09602f0d756812a19508bd0a6cd15b42
Raghunadh1809 <goneraghunadhareddy1212@gmail.com>
2025-11-15 02:05:02 +0530

    this is a commit for the UI

--- frotend/vite.config.js
+++ frotend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // or your backend port
+        target: "https://doc-summary-backend.vercel.app/", // or your backend port
         changeOrigin: true,
         secure: false,
       },
